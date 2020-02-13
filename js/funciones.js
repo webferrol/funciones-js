8f21b27912538cef00e3759ad20b64dc95381c8e
webferrol <info@webferrol.com>
2020-02-13 13:59:26 +0100

    Parte dos del ejercicio -Añadido función factorial -Función si me mira un número entero es positivo

--- js/funciones.js
+++ js/funciones.js
@@ -21,8 +21,29 @@ botones[0].addEventListener("click",e=>{
 });
 
 //ej2
-botones[1].addEventListener("click",e=>{
+//El elemento entero del cuadro de texto del factorial
+let elInputFactorial=document.querySelector("#f");
 
+const esNumeroEnteroPositivo=(numero)=>{
+ return (!isNaN(numero) && numero.length>0 && Number(numero)>0 && Number(numero)%1===0);
+}
+const calcularFactorial=(numeroEnteroPosivo)=>{
+  let arrayNumeros=[];
+  let ac=1;
+  for(let i=numeroEnteroPosivo;i>0;i--){
+    arrayNumeros.push(i);
+    ac*=i;//ac=ac*i;
+  }
+  alert(`${arrayNumeros.join("*")}=${ac}`);
+}
+
+botones[1].addEventListener("click",e=>{
+  if(esNumeroEnteroPositivo(elInputFactorial.value)){
+    //calculamos factorial
+    calcularFactorial(Number(elInputFactorial.value));
+  }else{
+    alert(`ERROR "[ ${elInputFactorial.value} ]" NO ES NÚMERO ENTERO POSITIVO`);
+  }
 });
 //ej3
 botones[2].addEventListener("click",e=>{
